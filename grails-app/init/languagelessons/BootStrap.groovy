c86eca7050a7bcfc78981e58ead2c238454077c3
Andrew Wilson <Andrew Wilson>
2017-01-17 11:17:29 -0600

    Fixing security and adding test users

--- grails-app/init/languagelessons/BootStrap.groovy
+++ grails-app/init/languagelessons/BootStrap.groovy
@@ -1,9 +1,45 @@
 package languagelessons
+import languagelessons.Role
+import languagelessons.User
+import languagelessons.UserRole
 
 class BootStrap {
 
     def init = { servletContext ->
+        
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+        def facultyRole = new Role(authority: 'ROLE_FACULTY').save()
+        def studentRole = new Role(authority: 'ROLE_STUDENT').save()
+        
+
+        if (!User.findByUsername('andrew@email.com') ) {
+            def u = new User(username: 'andrew@email.com', password: '#test#')
+            u.save()
+            UserRole.create u, adminRole
+        }
+
+        if (!User.findByUsername('lee@email.com') ) {
+            def u = new User(username: 'lee@email.com', password: '#test#')
+            u.save()
+            UserRole.create u, facultyRole
+        }
+        
+        if (!User.findByUsername('jon@email.com') ) {
+            def u = new User(username: 'jon@email.com', password: '#test#')
+            u.save()
+            UserRole.create u, facultyRole
+        }
+        
+         UserRole.withSession {
+         it.flush()
+         it.clear()
+      }
+
+      assert User.count() == 3
+      assert Role.count() == 3
     }
+
+    
     def destroy = {
     }
 }
