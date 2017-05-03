9213feaaa2b053618980cc2ec8dfb41998561aff
Alex <Alex@137.22.170.53>
2017-05-02 19:41:43 -0500

    Computed signature matches signature in post request

--- grails-app/controllers/languagelessons/OauthController.groovy
+++ grails-app/controllers/languagelessons/OauthController.groovy
@@ -8,76 +8,22 @@ class OauthController {
     
     def authorize() {
         
-//        TreeMap<String, String> fullMap = new TreeMap<String, String>();
-//        def xmlMap = request.getParameterMap()
-//        
-//        for(String key: xmlMap.keySet()) {
-//            fullMap.put(key, xmlMap.get(key))
-//        }
-//        
-//        fullMap.remove("oauth_signature")
-//        
-//        Set<Set<Map.Entry<String,String>>> pSet = ltiService.powerSet(fullMap.entrySet());
-//        
-//        println pSet.size();
-//        
-//        for(Set<Map.Entry<String,String>> s: pSet) {
-//            if (s.isEmpty())
-//                continue;
-//            
-//            TreeMap<String,String> curMap = new TreeMap<String,String>();
-//            for(Map.Entry<String,String> entry: s)
-//                curMap.add(entry.getKey(), entry.getValue());
-//            String secret = LtiService.generateOAuthSignature("POST", request.getRequestURL().toString(), "my-secret", curMap)
-//            println secret
-//            if(params.oauth_signature.equals(secret))
-//                println curMap;
-//        }
-//        
-//        TreeMap<String, String> oauthMap = new TreeMap<String, String>();
-//        
+        TreeMap<String, String> fullMap = new TreeMap<String, String>();
         def xmlMap = request.getParameterMap()
-//        
-        for(String key: xmlMap.keySet())
-        {
-//            if(key.startsWith("oauth") && !key.equals("oauth_signature")) {
-//                oauthMap.put(key, xmlMap.get(key))
-//                println "       " + key + oauthMap.get(key)
-//            }
-//            if(!key.equals("oauth_signature")) {
-//                fullMap.put(key, xmlMap.get(key))
-//            }
-//            
-            println key + ": " + xmlMap.get(key)
-        }
-//        
-//        println "\n\n\n"
-//        
-//        println LtiService.generateOAuthSignature("POST", request.getRequestURL().toString(), "my-secret", oauthMap)
-
-//        TreeMap<String, String> testMap = new TreeMap<String, String>();
-//        
-//        testMap.put("oauth_consumer_key", "key")
-//        testMap.put("oauth_nonce", "nonce")
-//        testMap.put("oauth_signature_method", "HMAC-SHA1")
-//        testMap.put("oauth_timestamp", "123456789")
-//        testMap.put("oauth_token", "token")
-//        
-//        println LtiService.generateOAuthSignature("POST", "http://example.com/wp-json/wp/v2/posts", "abcd", testMap)
-//
-//        TreeMap<String, String> setMap = new TreeMap<String, String>();
-//        setMap.put("1", "v1");
-//        setMap.put("2", "v2")
-//        setMap.put("3", "v3")
-//        
-//        Set<Set<Map.Entry<String,String>>> pSet = ltiService.powerSet(setMap.entrySet());
-//        
-//        for(Set<Map.Entry<String,String>> s: pSet) {
-//            println s
-//        
-//        }
-            
         
-        render "Siggy"
+        for(String key: xmlMap.keySet()) {
+            for(String value: xmlMap.get(key)) {
+               fullMap.put(key, value)
+            }
+        }
+        
+        fullMap.remove("oauth_signature")
+        
+        String sig = LtiService.generateOAuthSignature("POST", request.getRequestURL().toString(), "my-secret", fullMap)
+        
+        if(sig.equals(params.oauth_signature))
+            render "Sig"
+        else
+            render sig + "\n" + params.oauth_signature
     }
 }
--- grails-app/services/languagelessons/LtiService.groovy
+++ grails-app/services/languagelessons/LtiService.groovy
@@ -1,9 +1,9 @@
 package languagelessons
 
-import org.apache.commons.lang.RandomStringUtils
 
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
+import org.springframework.web.util.UriUtils
 
 /**
  * Utility to create OAuth 1 authorization headers.
@@ -14,73 +14,19 @@ import javax.crypto.spec.SecretKeySpec
  */
 
 class LtiService {
-    def test() {
-        return "string"
-    }
     
-    public static Set<Set<Map.Entry<String,String>>> powerSet(Set<Map.Entry<String,String>> originalSet) {
-        Set<Set<Map.Entry<String,String>>> sets = new HashSet<Set<Map.Entry<String,String>>>();
-        if (originalSet.isEmpty()) {
-            sets.add(new HashSet<Map.Entry<String,String>>());
-            return sets;
-        }
-        List<Map.Entry<String,String>> list = new ArrayList<Map.Entry<String,String>>(originalSet);
-        Map.Entry<String,String> head = list.get(0);
-        Set<Map.Entry<String,String>> rest = new HashSet<Map.Entry<String,String>>(list.subList(1, list.size()));
-        for (Set<Map.Entry<String,String>> set : powerSet(rest)) {
-            Set<Map.Entry<String,String>> newSet = new HashSet<Map.Entry<String,String>>();
-            newSet.add(head);
-            newSet.addAll(set);
-            sets.add(newSet);
-            sets.add(set);
-        }
-        return sets;
-    }
-    
-    public static String generateOAuthAuthorizationHeader(String method, String url, String consumerKey, String consumerSecret) {
-
-		generateOAuthAuthorizationHeader(method, url, consumerKey, consumerSecret, [:])
-	}
-    
-    public static String generateOAuthAuthorizationHeader(String method, String url, String consumerKey, String consumerSecret, Map additionalParameters) {
-
-        def signatureMethod = 'HMAC-SHA1'
-        def version = '1.0'
-
-        // Get timestamp in seconds
-        def timestamp = "${Math.round(new Date().getTime()/1000)}";
-
-        // OAuth nonce consists of 6 randomly generated characters, which must be unique for each request.
-        def nonce = RandomStringUtils.random(6, true, true)
-
-        def oAuthParameters = [
-                        oauth_consumer_key: consumerKey,
-                        oauth_nonce: nonce,
-                        oauth_signature_method: signatureMethod,
-                        oauth_timestamp: timestamp,
-                        oauth_version: version
-        ]
-
-        // Combine oAuth parameters and additional request parameters to generate signature
-        def signature = generateOAuthSignature(method, url, consumerSecret, oAuthParameters + additionalParameters)
-
-        "OAuth oauth_consumer_key=\"${consumerKey}\"," +
-                        "oauth_signature_method=\"${signatureMethod}\"," +
-                        "oauth_timestamp=\"${timestamp}\"," +
-                        "oauth_nonce=\"${nonce}\"," +
-                        "oauth_version=\"${version}\"," +
-                        "oauth_signature=\"${encode(signature)}\""
-        
-        return "success"
-    }
-
     public static String generateOAuthSignature(String method, String url, String consumerSecret, Map parameters) {
-        // Sort oAuth params & additional params alphabetically
-        def sortedParameters = parameters.sort { it.key }
         
-        for(String key:sortedParameters.keySet())
-            println key + ": " + sortedParameters.get(key)
-
+        TreeMap<String,String> encodeParams = new TreeMap<String,String>();
+        
+        // Each key and value needs to be encoded before being sorted and concatanated
+        for(String key: parameters.keySet()) {
+            encodeParams.put(encode(key), encode(parameters.get(key)));
+        }
+        
+        // Sort oAuth params & additional params alphabetically
+        def sortedParameters = encodeParams.sort { it.key }
+        
         def signatureParameters = ''
         sortedParameters.each {
                 signatureParameters += "${it.key}=${it.value}&"
@@ -90,22 +36,22 @@ class LtiService {
         signatureParameters = signatureParameters.substring(0, signatureParameters.length() - 1)
 
         def baseString = "${method}&${encode(url)}&${encode(signatureParameters)}"
-        
-        println "   " + baseString
 
         // Generate HMAC-SHA1
         def keySpec = new SecretKeySpec((consumerSecret + '&').bytes, 'HmacSHA1')
         def mac = Mac.getInstance('HmacSHA1');
         mac.init(keySpec)
         def calculatedBytes = mac.doFinal(baseString.getBytes('UTF-8'))
-
-        println "     " + calculatedBytes
+        
         // Base64 encode the HMAC
         return new String(Base64.encoder.encode(calculatedBytes))
     }
 
     private static String encode(String stringToEncode) {
-
-        URLEncoder.encode(stringToEncode, 'UTF-8')
+        String encodeStr = UriUtils.encode(stringToEncode, "UTF-8");
+        encodeStr.replaceAll('%7E', '~');
+        encodeStr.replaceAll('\\+', ' ');
+        
+        return encodeStr
     }
 }
